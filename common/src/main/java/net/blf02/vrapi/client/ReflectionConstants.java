167ee4ba0c6c41086b76409d77e300b530cd20d0
hammy275 <hammy275@gmail.com>
2023-06-27 20:21:58 -0400

    Hotswitch Vivecraft Support

--- common/src/main/java/net/blf02/vrapi/client/ReflectionConstants.java
+++ common/src/main/java/net/blf02/vrapi/client/ReflectionConstants.java
@@ -3,9 +3,16 @@ package net.blf02.vrapi.client;
 public class ReflectionConstants {
     // String constants
     public static final String VIVECRAFT_PACKAGE = "org.vivecraft";
+
+    // String constants for Optifine/pre-hotswitch Vivecraft
     public static final String VIVECRAFT_MATH_PACKAGE = VIVECRAFT_PACKAGE + ".utils.math";
     public static final String VIVECRAFT_PROVIDER_PACKAGE = VIVECRAFT_PACKAGE + ".provider";
     public static final String VIVECRAFT_GAMEPLAY_PACKAGE = VIVECRAFT_PACKAGE + ".gameplay";
+
+    // String constants for hotswitch Vivecraft
+    public static final String VIVECRAFT_CLIENT_VR_PACKAGE = VIVECRAFT_PACKAGE + ".client_vr";
+
+
     // Reflected class references
     public static Class<?> VRPlayerRaw = null;
     public static Class<?> VRDataRaw = null;
@@ -29,7 +36,20 @@ public class ReflectionConstants {
             Matrix4f = Class.forName(VIVECRAFT_MATH_PACKAGE + ".Matrix4f");
             VRSettings = Class.forName(VIVECRAFT_PACKAGE + ".settings.VRSettings");
             hasVivecraft = true;
-        } catch (ClassNotFoundException ignored) {}
+        } catch (ClassNotFoundException ignored) {
+            // Hotswitch Vivecraft locations
+            try {
+                VRPlayerRaw = Class.forName(VIVECRAFT_CLIENT_VR_PACKAGE + ".gameplay.VRPlayer");
+                VRDataRaw = Class.forName(VIVECRAFT_CLIENT_VR_PACKAGE + ".VRData");
+                VRDevicePoseRaw = VRDataRaw.getDeclaredClasses()[0];
+                MCVR = Class.forName(VIVECRAFT_CLIENT_VR_PACKAGE + ".provider.MCVR");
+                ControllerType = Class.forName(VIVECRAFT_CLIENT_VR_PACKAGE + ".provider.ControllerType");
+                ControllerType_ENUMS = ControllerType.getEnumConstants();
+                Matrix4f = Class.forName(VIVECRAFT_PACKAGE + ".common.utils.math.Matrix4f");
+                VRSettings = Class.forName(VIVECRAFT_CLIENT_VR_PACKAGE + ".settings.VRSettings");
+                hasVivecraft = true;
+            } catch (ClassNotFoundException ignored2) {}
+        }
     }
 
     public static boolean clientHasVivecraft() {
