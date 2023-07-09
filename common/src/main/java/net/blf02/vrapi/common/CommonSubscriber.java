2e4bb8520310101eb2187bf84850bdfbcb1e489f
hammy3502 <hammy275@gmail.com>
2023-07-09 12:57:32 -0400

    VR Switching Fix

--- common/src/main/java/net/blf02/vrapi/common/CommonSubscriber.java
+++ common/src/main/java/net/blf02/vrapi/common/CommonSubscriber.java
@@ -7,6 +7,7 @@ import net.minecraft.world.entity.player.Player;
 public class CommonSubscriber {
 
     public static void onPlayerDisconnect(Player player) {
+        // Also called from LeftVRPacket
         Tracker.playerToVR.remove(player.getGameProfile().getName());
         Tracker.playersInVR.remove(player.getGameProfile().getName());
     }
