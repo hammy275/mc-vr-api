7b3ee4ac65a114ce9b943e9ba8583797fa7baa3b
hammy3502 <hammy275@gmail.com>
2023-07-16 22:28:11 -0400

    Fix DebugSubscriber 1.18

--- common/src/main/java/net/blf02/vrapi/debug/DebugSubscriber.java
+++ common/src/main/java/net/blf02/vrapi/debug/DebugSubscriber.java
@@ -1,7 +1,7 @@
 package net.blf02.vrapi.debug;
 
 import net.blf02.vrapi.common.VRAPI;
-import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.world.entity.player.Player;
 
 public class DebugSubscriber {
@@ -10,10 +10,10 @@ public class DebugSubscriber {
         if (VRAPI.VRAPIInstance.playerInVR(player)) {
             VRAPI.VRAPIInstance.triggerHapticPulse(1, 0.05f,
                     null);
-            player.sendSystemMessage(Component.literal(
+            player.sendMessage(new TextComponent(
                     (player.isLocalPlayer() ? "Client" : "Server") + " reports in VR status: "
                     + VRAPI.VRAPIInstance.playerInVR(player)
-            ));
+            ), player.getUUID());
         }
     }
 }
