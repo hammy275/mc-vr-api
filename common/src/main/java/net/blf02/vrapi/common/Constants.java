7623db89699cafdd06c6ce2b1cc58581b778f7cf
hammy275 <hammy275@gmail.com>
2024-01-14 11:21:29 -0500

    Hopefully Fixed Hotswap Crashes + Version Bump

--- common/src/main/java/net/blf02/vrapi/common/Constants.java
+++ common/src/main/java/net/blf02/vrapi/common/Constants.java
@@ -3,7 +3,7 @@ package net.blf02.vrapi.common;
 public class Constants {
 
     // Version {major, minor, patch}
-    public static final int[] version = new int[]{3, 0, 6};
+    public static final int[] version = new int[]{3, 0, 7};
 
     // Debugging
     public static final boolean doDebugging = false;
